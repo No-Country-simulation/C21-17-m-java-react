3112a08809ac3e997a392b7a954387abd1bd6eb5
BernardoRoyo <Royo2193@gmail.com>
2024-10-10 23:59:24 -0300

    arreglitos

---
+++ src/main/java/com/example/MentorDigital/MentorDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.MentorDigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MentorDigital/MentorDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MentorDigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
