6229735adda0f59ba1b57049635409475ff77e92
SchrohSantiago <santiagoschroh@gmail.com>
2024-10-16 12:01:52 -0300

    first push microservice

---
+++ microservice-course/src/main/java/com/microservice/course/MicroserviceCourseApplication.java
@@ -0,0 +1,16 @@
+package com.microservice.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+ // El microservicio sera cliente de Eureka
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MicroserviceCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCourseApplication.class, args);
+	}
+
+}
